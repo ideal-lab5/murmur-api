33a8ca721de765790c18a6ade30e7abf09eb5c42
Juan Girini <juangirini@gmail.com>
2024-09-30 23:10:02 +0200

    feat: wip: fill up the endpoints

--- src/main.rs
+++ src/main.rs
@@ -18,10 +18,17 @@
 extern crate rocket;
 
 use bcrypt::hash_with_salt;
+use murmur::etf::runtime_types::bounded_collections::bounded_vec::BoundedVec;
+use murmur::etf::runtime_types::node_template_runtime::RuntimeCall;
+use murmur::MurmurStore;
 use rocket::http::Status;
 use rocket::http::{Cookie, CookieJar};
 use rocket::serde::json::Json;
+use rocket::{response::Responder, Request, Response};
 use serde::{Deserialize, Serialize};
+use std::fs::File;
+use std::io::Cursor;
+use subxt_signer::sr25519::dev;
 
 const SALT: &str = "your-server-side-secret-salt";
 
@@ -29,6 +36,102 @@ const SALT: &str = "your-server-side-secret-salt";
 struct LoginRequest {
 	username: String,
 	password: String,
+}
+
+#[derive(Serialize)]
+pub struct Payload<CallData> {
+	pallet_name: String,
+	call_name: String,
+	call_data: CallData,
+}
+
+// Implement the From trait for Payload to convert from TxPayload
+impl<C, D> From<murmur::TxPayload<C>> for Payload<D>
+where
+	// C: Clone,
+	D: for<'a> From<&'a C>,
+{
+	fn from(tx_payload: murmur::TxPayload<C>) -> Self {
+		Payload {
+			pallet_name: tx_payload.pallet_name().to_string(),
+			call_name: tx_payload.call_name().to_string(),
+			call_data: tx_payload.call_data().into(),
+		}
+	}
+}
+
+#[derive(Serialize, Clone)]
+struct Create {
+	root: Vec<u8>,
+	size: u64,
+	name: Vec<u8>,
+}
+
+impl<'a> From<&'a murmur::Create> for Create {
+	fn from(create: &'a murmur::Create) -> Self {
+		Create { root: create.root.clone(), size: create.size, name: create.name.0.clone() }
+	}
+}
+
+#[derive(Serialize)]
+struct CreateResponse {
+	payload: Payload<Create>,
+	store: MurmurStore,
+}
+
+impl<'r> Responder<'r, 'static> for CreateResponse {
+	fn respond_to(self, _: &'r Request<'_>) -> rocket::response::Result<'static> {
+		let json_response =
+			serde_json::to_string(&self).map_err(|_| Status::InternalServerError)?;
+		Response::build()
+			.header(rocket::http::ContentType::JSON)
+			.sized_body(json_response.len(), Cursor::new(json_response))
+			.ok()
+	}
+}
+
+#[derive(Serialize)]
+struct Proxy {
+	pub name: Vec<u8>,
+	pub position: u64,
+	pub hash: Vec<u8>,
+	pub ciphertext: Vec<u8>,
+	pub proof: Vec<Vec<u8>>,
+}
+
+impl<'a> From<&'a murmur::Proxy> for Proxy {
+	fn from(proxy: &'a murmur::Proxy) -> Self {
+		Proxy {
+			name: proxy.name.0.clone(),
+			position: proxy.position,
+			hash: proxy.hash.clone(),
+			ciphertext: proxy.ciphertext.clone(),
+			proof: proxy.proof.clone(),
+		}
+	}
+}
+
+#[derive(Serialize)]
+struct ProxyResponse {
+	payload: Payload<Proxy>,
+}
+
+impl<'r> Responder<'r, 'static> for ProxyResponse {
+	fn respond_to(self, _: &'r Request<'_>) -> rocket::response::Result<'static> {
+		let json_response =
+			serde_json::to_string(&self).map_err(|_| Status::InternalServerError)?;
+		Response::build()
+			.header(rocket::http::ContentType::JSON)
+			.sized_body(json_response.len(), Cursor::new(json_response))
+			.ok()
+	}
+}
+
+#[derive(Deserialize)]
+struct PrepareExecuteRequest {
+	name: String,
+	seed: String,
+	amount: u128,
 }
 
 #[post("/login", data = "<login_request>")]
@@ -44,33 +147,53 @@ async fn login(login_request: Json<LoginRequest>, cookies: &CookieJar<'_>) -> &'
 }
 
 #[post("/create")]
-async fn create(cookies: &CookieJar<'_>) -> Result<String, Status> {
+async fn create(cookies: &CookieJar<'_>) -> Result<CreateResponse, Status> {
 	check_cookie(cookies, |username, seed| async {
-		murmur::create(
+		let (create_payload, store) = murmur::create(
 			username.to_string(),
 			seed.to_string(),
 			[1; 32],
 			vec![1, 2, 3],
 			vec![4, 5, 6, 7],
-		)
-		.await;
-		"create mmr called".to_string()
+		);
+		CreateResponse { payload: create_payload.into(), store }
 	})
 	.await
 	.map_err(|_| Status::Forbidden)
 }
 
-#[post("/execute")]
-async fn execute(cookies: &CookieJar<'_>) -> Result<String, Status> {
-	check_cookie(cookies, |_username, _seed| async { "execute called".to_string() })
-		.await
-		.map_err(|_| Status::Forbidden)
+#[post("/prepare_execute", data = "<request>")]
+async fn prepare_execute(
+	cookies: &CookieJar<'_>,
+	request: Json<PrepareExecuteRequest>,
+) -> Result<ProxyResponse, Status> {
+	check_cookie(cookies, |_username, _seed| async {
+		let request = request.into_inner();
+		let current_block_number = 1; // TODO: get from runtime
+		let bob = dev::bob().public_key();
+		let balance_transfer_call =
+			RuntimeCall::Balances(murmur::etf::balances::Call::transfer_allow_death {
+				dest: subxt::utils::MultiAddress::<_, u32>::from(bob),
+				value: request.amount,
+			});
+		let proxy_payload = murmur::prepare_execute(
+			request.name.into_bytes(),
+			request.seed.into_bytes(),
+			current_block_number,
+			load_mmr_store(),
+			balance_transfer_call,
+		)
+		.await;
+		ProxyResponse { payload: proxy_payload.into() }
+	})
+	.await
+	.map_err(|_| Status::Forbidden)
 }
 
-async fn check_cookie<'a, F, Fut>(cookies: &'a CookieJar<'_>, callback: F) -> Result<String, ()>
+async fn check_cookie<'a, F, Fut, R>(cookies: &'a CookieJar<'_>, callback: F) -> Result<R, ()>
 where
 	F: FnOnce(&'a str, &'a str) -> Fut,
-	Fut: std::future::Future<Output = String>,
+	Fut: std::future::Future<Output = R>,
 {
 	let username = cookies.get("username");
 	let seed = cookies.get("seed");
@@ -90,7 +213,15 @@ fn derive_seed(password: &str, username: &str) -> String {
 		.to_string()
 }
 
+fn load_mmr_store() -> MurmurStore {
+	// TODO: load from DB
+	let mmr_store_file = File::open("mmr_store").expect("Unable to open file");
+	let data: MurmurStore = serde_cbor::from_reader(mmr_store_file).unwrap();
+
+	data
+}
+
 #[launch]
 fn rocket() -> _ {
-	rocket::build().mount("/", routes![login, create, execute])
+	rocket::build().mount("/", routes![login, create, prepare_execute])
 }
